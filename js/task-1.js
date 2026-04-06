226a55cc02427061af3c9c6691e67a7cd289256f
Lana_Huerzeler <huerzeler.lana@gmail.com>
2026-04-06 03:06:59 +0200

    hw7

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector("#categories");
+const items = categories.querySelectorAll(".item");
+
+// 1. Кількість категорій
+console.log(`Number of categories: ${items.length}`);
+
+// 2. Для кожної категорії
+items.forEach(item => {
+  const title = item.querySelector("h2").textContent;
+  const elements = item.querySelectorAll("li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elements}`);
+});
